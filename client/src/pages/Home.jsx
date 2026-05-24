709ec6ed65eed93f57ab0ede9a26a07b3ea022c2
Himanshu Narware <83147410+HimanshuNarware@users.noreply.github.com>
2026-05-24 13:59:52 +0530

    fix dep prob

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import Footer from './Footer';
