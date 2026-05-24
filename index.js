cd68c08a09dcdcd536efde3f3dc5936ad54e6673
Himanshu Narware <83147410+HimanshuNarware@users.noreply.github.com>
2026-05-24 22:37:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin:'*',
     credentials: true // Required for cookies to be sent cross-origin
 }));
 
